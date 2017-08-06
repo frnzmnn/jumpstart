fd581dc5b612969c8e1578347e55831c4c8e7a17
frnzmnn <naumann_franziska@yahoo.de>
2017-08-06 15:15:56 +0200

    Hide navigation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
       $('.carousel').slick({
-        autoplay: true;
-        autoplaySpeed: 4000;
-        arrows: false;
-        pauseOnFocus: false;
-        pauseOnHover: false;
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+      });
+    });
+
+$(document).ready(function(){
+      $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+          $("navbar").addClass("hide");
+        } else {
+          $("navbar").removeClass("hide");
+        }
       });
     });
