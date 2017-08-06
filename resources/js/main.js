11b0af239a418be7d14a54f8ab0b031ed7ddef29
frnzmnn <naumann_franziska@yahoo.de>
2017-08-06 14:44:01 +0200

    Add carousel

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      $('.carousel').slick({
+        autoplay: true;
+        autoplaySpeed: 4000;
+        arrows: false;
+        pauseOnFocus: false;
+        pauseOnHover: false;
+      });
+    });
